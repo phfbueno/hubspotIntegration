cc46dbec241b182cc7e66855ff04fdd4bd0cd56d
Paulo Bueno <phf.bueno@gmail.com>
2025-04-25 17:52:54 -0300

    commit com a estrutura do projeto

---
+++ src/test/java/com/github/phfbueno/hubspotintegration/HubspotIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.phfbueno.hubspotintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubspotIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
